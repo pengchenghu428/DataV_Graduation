48f59a22afc5efd49cd92f25eff77b3ab4a65f7f
pengchenghu428 <1447419216@qq.com>
2021-01-20 19:06:13 +0800

    2021/01/19->pengchenghu: flask-Bootstrap测试

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
